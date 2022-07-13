a10886baa4a0dbfce45c7818a72eeb1baff244b4
RysteQ <34135554+RysteQ@users.noreply.github.com>
2022-07-13 05:18:09 +0300

    Added AddTwoNumbers.c and rename printf folder to Day 1

---
+++ Day 1/AddTwoNumbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int firstNumber = 10;
+    int secondNumber = 6;
+    int result = firstNumber + secondNumber;
+
+    printf("The result is %i\n", result);
+
+    return 0;
+}
