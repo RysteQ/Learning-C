6b2174a9221578060f3a10bd354b2a23b03551d6
RysteQ <34135554+RysteQ@users.noreply.github.com>
2022-07-14 02:35:14 +0300

    Create addTenNumbers.c
    I am finally in for loops. I should note (although you can see it in the code) that the book did not go through functions or input yet, I just found the examples quite boring so I made my own exercise.

---
+++ Day 2/addTenNumbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int getInt();
+
+int main() {
+    int total = 0;
+
+    for (int i = 0; i < 10; i++) {
+        printf("Number %i -> ", i);
+        total += getInt();
+    }
+
+    printf("The total sum of the numbers is %i", total);
+
+    return 0;
+}
+
+// I did not learn this from the book, this is something I remember doing
+int getInt() {
+    int toReturn;
+
+    scanf("%i", &toReturn);
+
+    return toReturn;
+}
