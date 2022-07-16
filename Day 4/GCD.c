74fc2c001e36e920157fb7d4e394023efa30e18d
RysteQ <34135554+RysteQ@users.noreply.github.com>
2022-07-17 00:46:06 +0300

    Create GCD.c

---
+++ Day 4/GCD.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#define TRUE 1
+
+int GCD(int numberOne, int numberTwo);
+int GetInt(void);
+
+int main() {
+    printf("Enter number one -> ");
+    int numberOne = GetInt();
+
+    printf("Enter number two -> ");
+    int numberTwo = GetInt();
+
+    printf("The greatest common dividor is %i\n", GCD(numberOne, numberTwo));
+
+    return 0;
+}
+
+int GCD(int numberOne, int numberTwo) {
+    int remainder;
+
+    while (TRUE) {
+        remainder = numberOne % numberTwo;
+
+        if (remainder == 0)
+            break;
+
+            numberOne = numberTwo;
+            numberTwo = r;
+    }
+
+    return numberTwo;
+}
+
+int GetInt(void) {
+    int toReturn;
+
+    scanf("%i", &toReturn);
+
+    return toReturn;
+}
