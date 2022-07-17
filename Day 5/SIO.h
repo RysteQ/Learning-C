b473850aaf1319d19a2802cf5d6d46391b32a489
RysteQ <34135554+RysteQ@users.noreply.github.com>
2022-07-17 04:22:41 +0300

    Create SIO.h

---
+++ Day 5/SIO.h
@@ -0,0 +1,93 @@
+#ifndef _SIO_h
+#define _SIO_h
+
+#include <stdio.h>
+
+#pragma region declareFuncs
+short GetShort(void);
+int GetInt(void);
+long GetLong(void);
+char GetChar(void);
+float GetFloat(void);
+double GetDouble(void);
+long double GetLongDouble(void);
+
+unsigned int GetUnsignedInt(void);
+unsigned long GetUnsignedLong(void);
+#pragma endregion declareFuncs
+
+#pragma region funcs
+short GetShort(void) {
+    short toReturn;
+
+    scanf("%hi", &toReturn);
+
+    return toReturn;
+}
+
+int GetInt(void) {
+    int toReturn;
+
+    scanf("%i", &toReturn);
+
+    return toReturn;
+}
+
+long GetLong(void) {
+    long toReturn;
+
+    scanf("%li", &toReturn);
+
+    return toReturn;
+}
+
+char GetChar(void) {
+    char toReturn;
+
+    scanf("%c", &toReturn);
+
+    return toReturn;
+}
+
+float GetFloat(void) {
+    float toReturn;
+
+    scanf("%f", &toReturn);
+
+    return toReturn;
+}
+
+double GetDouble(void) {
+    double toReturn;
+
+    scanf("%lf", &toReturn);
+
+    return toReturn;
+}
+
+long double GetLongDouble(void) {
+    long double toReturn;
+
+    scanf("%Lf", &toReturn);
+
+    return toReturn;
+}
+
+unsigned int GetUnsignedInt(void) {
+    unsigned int toReturn;
+
+    scanf("%u", &toReturn);
+
+    return toReturn;
+}
+
+unsigned long GetUnsignedLong(void) {
+    unsigned long int toReturn;
+
+    scanf("%lu", &toReturn);
+
+    return toReturn;
+}
+#pragma endregion funcs
+
+#endif
