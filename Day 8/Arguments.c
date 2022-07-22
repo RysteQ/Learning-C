7732df20725d89f963b4fad4034dbfc537dbe8dd
RysteQ <34135554+RysteQ@users.noreply.github.com>
2022-07-22 06:05:31 +0300

    Create Arguments.c

---
+++ Day 8/Arguments.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    if (argc == 1 || argc > 2) {
+        printf("Invalid amount of arguments\n");
+        return -1;
+    }
+
+    printf("Argument: %s\n", argv[1]);
+
+    return 0;
+}
